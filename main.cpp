89a645a9c8d2b54ae77fd2f61709cb70d41766f4
Phineas <phowell2@stevens.edu>
2021-09-08 19:37:54 -0400

    5

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    // Initializing phase 1
+    // Initializing phase I
     int m1x; int m1y; int m2x; int m2y;
     int maxrandomvalue = 99;
 
@@ -14,39 +14,71 @@ int main() {
 
     // Making sure the input is valid
     if((m1y != m2x) || (m1x != m2y)){
-        cout << "\nSorry! the matrices you entered are not supported!";
+        cout << "\nSorry! The dot product for the matrices you entered is not calculable!";
         return 1;
     }
 
-    // Initializing phase 2
+    // Initializing phase II
     int m1[m1x][m1y];
     int m2[m2x][m2y];
     int lox; int loy;
+    int hix; int hiy;
 
-    // Filling the first array
+    // Filling the arrays with random numbers
     for (int x = 0; x < m1x; x++){for (int y = 0; y < m1y; y++){
         m1[x][y] = rand()%maxrandomvalue;
     }}
-
-    // Filling the second array
     for (int x = 0; x < m2x; x++){for (int y = 0; y < m2y; y++){
-            m2[x][y] = rand()%maxrandomvalue;
-        }}
+        m2[x][y] = rand()%maxrandomvalue;
+    }}
 
-    // Determining the size of the resulting array and initializing it
-    if (m1x < m2x){ lox = m1x;}else{ lox = m2x;}
-    if (m1y < m2y){ loy = m1y;}else{ loy = m2y;}
+    // Determining his and los
+    if (m1x < m2x){
+        lox = m1x;
+        hix = m2x;
+    }else{
+        lox = m2x;
+        hix = m1x;
+    }
+    if (m1y < m2y){
+        loy = m1y;
+        hiy = m2y;
+    }else{
+        loy = m2y;
+        hiy = m1y;
+    }
 
-    // Testing to make sure lox and loy are correctly set
-    cout << "The lowest x value is " << lox << ", and the lowest y value is " << loy << ".";
-
-    // Initializing the result
+    // Initializing phase III
     int result[lox][loy];
 
-    /* Computing the dot product
-    for(int x = 0; x < lox; x++){for( int y = 0; y < loy; y++){
+    // multiplying the matrices DOES NOT WORK
+    for(int x = 0; x > hix; x++){ for(int y = 0; y > hiy; y++){ for(int z = 0; z > lox; z++){
+        result[x][y] = m1[x][z]*m2[z][y];
+    }}}
 
-    }}
-    */
+    // Printing
+    cout << "Matrix 1: \n";
+    for (int x = 0; x < m1x; x++){
+        for (int y = 0; y < m1y; y++){
+            cout << m1[x][y] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "Matrix 2: \n";
+    for (int x = 0; x < m2x; x++){
+        for (int y = 0; y < m2y; y++){
+            cout << m2[x][y] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "Result: \n";
+    for (int x = 0; x < lox; x++){
+        for (int y = 0; y < loy; y++){
+            cout << result[x][y] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
     return 0;
 }
